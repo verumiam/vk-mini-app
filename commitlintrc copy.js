b81bfbdcc00c646ce3a16816ce9923d0b594b3c7
Danil Grigoruk <danil_grigoruk@mail.ru>
2024-03-12 21:49:45 +0300

    Delete commitlintrc copy.js

--- commitlintrc copy.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [2, 'always', ['app', 'widgets', 'features', 'entities', 'shared', 'pages']],
-  },
-};
