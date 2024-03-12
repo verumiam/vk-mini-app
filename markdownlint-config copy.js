0fadfaeeb20d9a5e16c2e865834b0366d06c5694
Danil Grigoruk <danil_grigoruk@mail.ru>
2024-03-12 21:49:55 +0300

    Delete markdownlint-config copy.js

--- markdownlint-config copy.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  'first-line-h1': false,
-  'line-length': false,
-  'no-inline-html': {
-    allowed_elements: ['summary', 'details', 'img'],
-  },
-};
